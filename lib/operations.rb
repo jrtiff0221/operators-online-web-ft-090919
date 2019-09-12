1f84837050c4e39de6612b174f99ae08cc523869
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-12 01:01:32 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,8 @@
 def unsafe?(speed)
   if speed > 60 then 
-    speed == "unsafe"
+    return true
   elsif speed < 40
-    speed == "unsafe"
+    spe
   elsif speed > 40 && speed < 60
     speed = "safe"
    
